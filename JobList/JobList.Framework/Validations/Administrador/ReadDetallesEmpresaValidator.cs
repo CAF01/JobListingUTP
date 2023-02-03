e3f328884140f9a7d76d7236aeabd2bdf67e3602
CAF01 <Christian_pria@hotmail.com>
2023-02-03 16:53:39 -0600

    Validaciones Administrador

---
+++ JobList/JobList.Framework/Validations/Administrador/ReadDetallesEmpresaValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using JobList.Entities.Helpers;
+using JobList.Entities.Requests;
+
+namespace JobList.Framework.Validations.Administrador
+{
+    public class ReadDetallesEmpresaValidator : AbstractValidator<ReadDetallesEmpresaRequest>
+    {
+        public ReadDetallesEmpresaValidator()
+        {
+            RuleFor(data => data.idUsuarioEmpresa).NotNull().NotEmpty().GreaterThan(ValidatorHelper.ZERO);
+        }
+    }
+}
---
+++ JobList/JobList.Framework/Validations/Administrador/ReadOfertasPublicadasEmpresaValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using JobList.Entities.Helpers;
+using JobList.Entities.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobList.Framework.Validations.Administrador
+{
+    public class ReadOfertasPublicadasEmpresaValidator : AbstractValidator<ReadOfertasPublicadasEmpresaRequest>
+    {
+        public ReadOfertasPublicadasEmpresaValidator()
+        {
+            RuleFor(data => data.idUsuarioEmpresa).NotNull().NotEmpty().GreaterThan(ValidatorHelper.ZERO);
+        }
+    }
+}
